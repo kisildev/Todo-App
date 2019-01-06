588da8198c2b61367a9d5d15096cabdcb55fc2ed
kisildev <sanya.kisil96@gmail.com>
2019-01-06 21:24:17 +0200

    material ui added (buttons)

--- src/components/to-do-list-item/todo-list-item.js
+++ src/components/to-do-list-item/todo-list-item.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './todo-list-item.css';
 
-const TodoListItem = ({label, important}) => {
-    const style = {
-        color: important ? '#f44336' : null, 
-        fontWeight: important ? '500' : null 
+class TodoListItem extends Component {
+    render() {
+        const {label, important = false} = this.props;
+        const style = {
+            color: important ? '#f44336' : null, 
+            fontWeight: important ? '500' : null 
+        };
+        return (
+            <div className="todo-list-item-inner">
+                <div className="todo-item-text" style={style}>{label}</div>
+                <div className="button-group">
+                    <button className="btn btn--remove">
+                        <i className="material-icons small">delete</i>
+                    </button>
+                    <button className="btn btn--highlight">
+                        <i className="material-icons small">priority_high</i>
+                    </button>
+                </div>
+            </div>
+        ); 
     };
-    return <span style={style}>{label}</span>;
 }
 
 export default TodoListItem;
