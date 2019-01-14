6f07df8e0fe1601221a20c232827a98691be1c76
kisildev <sanya.kisil96@gmail.com>
2019-01-14 09:35:43 +0200

    push last changes

--- src/components/to-do-list-item/todo-list-item.js
+++ src/components/to-do-list-item/todo-list-item.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
+    constructor() {
+        super();
+        this.onLabelClick = () => console.log(`Done ${this.props.label}`)
+    }
+
     render() {
         const {label, important = false} = this.props;
         const style = {
@@ -10,7 +15,7 @@ export default class TodoListItem extends Component {
         };
         return (
             <div className="todo-list-item-inner">
-                <div className="todo-item-text" style={style}>{label}</div>
+                <div className="todo-item-text" style={style} onClick={ this.onLabelClick } >{label}</div>
                 <div className="button-group">
                     <button className="btn btn--remove">
                         <i className="material-icons small">delete</i>
